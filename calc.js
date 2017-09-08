e6cb9fa3c66bb7a1455afcb8cc96e57c42b1502e
gazayas <g-zayas@hotmail.com>
2017-09-08 18:26:04 +0900

    ちょっとしたリファクトリング

--- calc.js
+++ calc.js
@@ -5,30 +5,24 @@ window.onload = function () {
   $need_to_renew = false;
   $screen = document.getElementById('output'); // should probz change this
   $last_button_pressed = "";
+  $last_button_pressed_html = "";
 
   var buttons = document.getElementsByTagName('button');
 
   // Add onclick to all the buttons
-  for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', input, false);
-  }
+  for (var i = 0; i < buttons.length; i++) { buttons[i].addEventListener('click', input, false); }
 
   // Set value for all number buttons
-  var nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
-  var x = 0;
   for (var i = 0; i < buttons.length; i++) {
     if (buttons[i].className.match(/num/)) {
-      buttons[i].value = nums[x];
-      x++;
+      buttons[i].value = buttons[i].innerHTML;
     }
   }
 }
 
 function input() {
   // reset operator from black background if it was pressed last
-  if ($last_button_pressed == "operator") {
-    $last_button_pressed_html.style.background = "";
-  }
+  if ($last_button_pressed == "operator") { $last_button_pressed_html.style.background = ""; }
 
   var button = window.event.target;
   var key_code = window.event.keyCode;
@@ -46,39 +40,38 @@ function input() {
     }
 
     switch(key_code) {
-      case 13:
+      case 13: // Enter key
         button.id = "equals";
         break;
-      case 8:
-        // Same as 67
-      case 67:
+      case 8: // Backspace
+      case 67: // C
         button.id = "clear";
         break;
-      case 188:
+      case 188: // <
         button.className = "delete";
         button.id = "delete";
         break;
-      case 187:
+      case 187: // +
         button.className = "operator";
         button.id = "add";
         break;
-      case 189:
+      case 189: // -
         button.className = "operator";
         button.id = "subtract";
         break;
-      case 222:
-      case 88:
+      case 222: // *
+      case 88: // x
         button.className = "operator";
         button.id = "multiply";
         break;
-      case 191:
+      case 191: // /
         button.className = "operator";
         button.id = "divide";
         break;
-      case 190:
+      case 190: // .
         button.id = "decimal";
         break;
-      case 77:
+      case 77: // M
         button.id = "memory";
         button.class = "mem";
         break;
@@ -90,13 +83,13 @@ function input() {
     $need_to_renew = false;
   }
 
+  // TODO: switch文に変えなさい。ここら辺はかなり汚いから綺麗にしてください
   // Limit 入力 length to 20
   if(button.className.match(/num/) && $screen.innerHTML.length < 20) {
     $screen.innerHTML += button.value;
     $current_val = $screen.innerHTML;
   } else if (button.className.match(/operator/)){
     button.style.background = "black"; // TODO: doesnt happen on keyboard press. get element and 実装してください
-
     if ($last_button_pressed.match(/num/) && $prev_val != "") {
       console.log($last_button_pressed)
       calculate();
@@ -105,7 +98,6 @@ function input() {
     } else {
       $prev_val = $current_val;
       $current_val = "";
-
       // TODO: Instead of clearing the screen, do $need_to_renew = true and just put the operator on the screen
       $screen.innerHTML = $current_val;
     }
@@ -119,7 +111,6 @@ function input() {
     if ($current_val.match(/^\-/)) {
       $current_val = $current_val.split('').splice(1);
       $current_val = $current_val.join('');
-      console.log("current: " + $current_val);
     } else {
       $current_val = "-" + $current_val;
     }
@@ -155,13 +146,12 @@ function input() {
     }
   }
 
+  // TODO: $last_button_pressed should be html object instead of string
   $last_button_pressed = button.className.split(' ').pop();
   $last_button_pressed_html = button;
-  console.log($last_button_pressed);
 
-  // Take focus off button after pressing it.
-  // Without this, if you press a button and then go to type a key,
-  // the BUTTONS action will get triggered instead.
+  // Take focus off button after pressing it so same action doesn't occur with different button
+  // Without this, if you press a button and then go to type a key, the BUTTONS action will get triggered instead.
   button.blur();
 }
 
@@ -169,11 +159,8 @@ function calculate() {
   $need_to_renew = true;
 
   // Cast values to integers
-  console.log("prev val: " + $prev_val);
   $current_val = Number($current_val);
   $prev_val = Number($prev_val);
-
-  console.log($current_val + $operator + $prev_val);
 
   switch($operator) {
     case "add":
@@ -198,6 +185,3 @@ function calculate() {
 document.onkeyup = function(e) {
   input();
 }
-
-/* TODO: Bug - $need_to_renew isn't applied to all the necessary places,
- so sometimes digits can be added to numbers that they shouldnt be added to. */
